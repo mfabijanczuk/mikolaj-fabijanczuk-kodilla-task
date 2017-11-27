7d390436b9a93626c2ef312b0b3526b2e8e32e34
“mfabijanczuk <mikolaj.fabijanczuk@gmail.com>
2017-11-27 01:24:53 +0100

    Moduł 15.2

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -7,8 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TaskApplicationTests {
-
+public class TasksApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
